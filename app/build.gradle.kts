caeb375424104c3741845b99b88ed9381d9a0bba
ErbKqlx <Error5455@yandex.ru>
2025-02-03 10:58:05 +0500

    Добавление панели с прокруткой

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.bitsandpizzas"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.bitsandpizzas"
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
